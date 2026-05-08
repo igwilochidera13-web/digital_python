555c04d2ec4487d6473e45642a9c1575052cb6e1
igwilochidera13-web <igwilochidera13@gmail.com>
2026-05-08 06:31:42 -0700

    done

--- exercise.py
+++ exercise.py
@@ -9,7 +9,32 @@ Exam score
 Whether they have a valid ID card (yes or no)
 """
 fullname = input("Enter your full name: ");
-age = input("Enter your age: ");
-exam_score = input("Enter examination score: ");
+age = int(input("Enter your age: ")); # 17
+exam_score = int(input("Enter examination score: "));
 has_valid_id_card = input("Do you have Valid ID card?: ");
 
+# Use typecasting where necessary.
+
+# exam_score = 90
+
+"""
+    int()
+    float()
+    bool()
+    str()
+"""
+
+"""
+    Use mathematical operators to:
+
+Add 5 bonus points to the exam score # 95
+Calculate how many years remain before the student turns 30
+"""
+
+final_score =exam_score + 5; # exam_score = exam_score + 5 - 90
+
+remaining_year = 30 - age; # 13
+
+print(final_score >= 70) # True
+
+print(age > 18)
